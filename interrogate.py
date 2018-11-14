b96416aceefea4c9a62e6dee668d1e4d75a97a75
Robbie Jennings <robbie.jennings97@gmail.com>
2018-11-14 16:26:28 +0000

    added more extensive querying for data to be put in database

--- interrogate.py
+++ interrogate.py
@@ -1,6 +1,38 @@
 from github import Github
 
 g = Github("4986e75bbfff142ff1550dcbca813ba0d6d872e3")
+users = []
 
-for repo in g.get_user().get_repos():
-    print(repo.name)
+source = "RobbieJennings"
+
+sourceUser = g.get_user(source)
+
+if(sourceUser not in users):
+    users.append(sourceUser)
+
+for follower in sourceUser.get_following():
+    if(follower not in users):
+        users.append(follower)
+
+for following in sourceUser.get_followers():
+    if(following not in users):
+        users.append(following)
+
+for user in users:
+    username = user.login
+    for repo in user.get_repos():
+        numCommits = repo.get_commits().totalCount
+        numBranches = repo.get_branches().totalCount
+        numContributors = repo.get_contributors().totalCount
+        size = repo.size
+        for language in repo.get_languages():
+            print(username)
+            print(language)
+            print(size)
+            print(numCommits)
+            print(numBranches)
+            print(numContributors)
+            print()
+        print("----------")
+    print("++++++++++")
+    # Put this data in database
